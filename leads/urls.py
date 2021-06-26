5bdbb5c6aa4bbcec99a735d3d6ecf761bdcc1282
chinthan11shah <chinthanshah@gmail.com>
2021-06-26 07:10:36 +0530

    Basic UI completed

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import lead_list, lead_details
+from .views import lead_delete, lead_list, lead_details, lead_create, lead_update
 
 app_name = "leads"
 
 urlpatterns = [
-    path('', lead_list),
-    path('<pk>/', lead_details)
+    path('', lead_list, name='lead-list'),
+    path('create/', lead_create, name='lead-create'),
+    path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('<int:pk>/delete/', lead_delete, name='lead-delete'),
+    path('<int:pk>/', lead_details, name='lead-detail'),
 ]
